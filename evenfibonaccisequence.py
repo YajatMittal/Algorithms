ca5d5c4695f64491b32858eaa5e6f5e2d5a410bf
Yajat Mittal <59126036+YajatMittal@users.noreply.github.com>
2020-10-13 18:47:52 -0400

    Add files via upload

---
+++ evenfibonaccisequence.py
@@ -0,0 +1,13 @@
+numbers = [1,2]
+even_numbers = []
+
+for x in range(0,4000001):
+  for a in numbers:
+    addition = numbers[x] + numbers[x+1]
+    numbers.append(addition)
+    
+    if a % 2 == 0:
+      even_numbers.append(a)
+
+print(even_numbers)
+
