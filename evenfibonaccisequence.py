052cc1337ad487ef5891d1d70e239f7c5be28c9b
Yajat Mittal <59126036+YajatMittal@users.noreply.github.com>
2020-10-13 19:44:36 -0400

    Update evenfibonaccisequence.py

--- evenfibonaccisequence.py
+++ evenfibonaccisequence.py
@@ -1,13 +1,16 @@
+#here are the two most important variables for the fibonacci sequence
 numbers = [1,2]
 even_numbers = []
 
+#here is a for loop running until the fibonacci sequence's value exceeds 4 million
 for x in range(0,4000000):
-  for a in numbers:
     addition = numbers[x] + numbers[x+1]
     numbers.append(addition)
     
-    if a % 2 == 0:
-      even_numbers.append(a)
+    #here I am appending the even number values in the fibonacci sequence into a list
+    if x % 2 == 0:
+      even_numbers.append(x)
 
+#this is thw last step where I am finding the sum of the even numbers
 print(even_numbers)
 
