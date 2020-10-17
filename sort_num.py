8fbc95d431fba6c60342ffb7acb48a0cce41a734
Yajat Mittal <59126036+YajatMittal@users.noreply.github.com>
2020-10-16 22:07:14 -0400

    Add files via upload

---
+++ sort_num.py
@@ -0,0 +1,20 @@
+def bubble_sort(alist):
+  counter = 1
+  while counter != len(alist):
+    for x in range(0, len(alist)-1):
+        val = x + 1
+        if alist[x] > alist[val]:
+            alist.insert(x, alist[val])
+            alist.pop(x+2)
+        else:
+            counter += 1
+  print(alist)
+  
+    
+    
+
+bubble_sort([5,8,3])
+
+
+
+
