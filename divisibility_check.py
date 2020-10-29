14285a5250d36f7328ca3dab00621f6935adf3fd
Yajat Mittal <59126036+YajatMittal@users.noreply.github.com>
2020-10-28 23:38:33 -0400

    Update divisibility_check.py

--- divisibility_check.py
+++ divisibility_check.py
@@ -1,19 +1,9 @@
-def divisibility_check():
-    active = True
+def divisibility_check(num):
     divisible_by = []
-    while active:
-        try:
-            num = int(input("Enter a number:"))
-        except:
-           print("Invalid!")
-           continue
-
-        else:
-            active = False
-    for x in range(1,10):
+    for x in range(1,num+1):
         if num %  x == 0:
             divisible_by.append(x)
     
     print(f"{num} is divisble by {str(divisible_by)}")
 
-divisibility_check()
+divisibility_check(100)
