048ba818bc78454c0e73add7897f924a9309e352
Yajat Mittal <59126036+YajatMittal@users.noreply.github.com>
2020-11-02 22:51:30 -0500

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,21 +1,13 @@
 def factorial():
-    factorial_num = int(input("Please enter a number -->"))
-    numbers = [x for x in range(1,factorial_num+1)]
+    factorial_num = int(input("Please enter a number:"))
     
     if factorial_num == 0:
         print("The factorial of 0 is 1")
-    
     elif factorial_num < 0:
-        print("Factorial of negatives don't exist")
-
+        print("Factorial of negatives don't exist")    
     else:
-        while len(numbers) != 1:
-            calculate = numbers[0] * numbers[1]
-            numbers.pop(0)
-            numbers.pop(0)
-            numbers.append(calculate)
-
-        print(f"The factorial of {factorial_num} is {numbers[0]}")
-    
-
+        for x in range(1,factorial_num):
+            factorial_num *= x
+        print(factorial_num)
+        
 factorial()
