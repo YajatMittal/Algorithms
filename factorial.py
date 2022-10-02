11e1d53866f4fb98098c038eb16b44681cb5e350
Yajat Mittal <59126036+YajatMittal@users.noreply.github.com>
2022-10-01 23:05:55 -0400

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,11 +1,11 @@
- def factorial(n):
-  if n > 1: 
-    n *= factorial(n-1)
-  else:
-    if n == 0:
-      print("The factorial of 0 is 1")
-    elif n < 0:
-      print("Factorial of negatives don't exist")  
-  return n
+def factorial(n):
+    if n > 1: 
+        n *= factorial(n-1)
+    else:
+        if n == 0:
+            print("The factorial of 0 is 1")
+        elif n < 0:
+            print("Factorial of negatives don't exist")  
+    return n
 
 print(factorial(5))
