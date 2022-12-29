b2248247409277e1be889a5595098467fcf76560
Yajat Mittal <59126036+YajatMittal@users.noreply.github.com>
2022-12-29 17:31:48 -0500

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,4 +1,3 @@
-'''
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for num1 in nums:
@@ -7,21 +6,3 @@ class Solution:
                     num_ind = nums.index(num1)
                     nums[nums.index(num1)] = None
                     return [num_ind,nums.index(num2)]
-'''
-
-class Solution:
-    def twoSum(self, nums, target):
-        for n in range(0,len(nums)):
-          if n + 1 > len(nums)-1:
-              break
-          if n == n + 1:
-            if n + 2 > len(nums)-1:
-                  break 
-            if nums[n] + nums[n+2] == target:
-                print([n,n+2]) 
-          else:
-            if nums[n] + nums[n+1] == target:
-              print([n,n+1])
-              
-leet = Solution()
-print(leet.twoSum([2,5,11],10))         
